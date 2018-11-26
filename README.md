93f3e01d66da9598e44bc72cd79c444304e46b67
Jacob Mines | Mines Eye Media <jakemines@gmail.com>
2018-11-27 00:57:29 +0800

    Commit#3 - Coded JS Parallax Effect and tied into heroSection bg.
    Commit #3 - Coded JS Parallax Effect and tied into heroSection bg.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Bootstrap 2 Wordpress v4.0
 Commit #1 - Initial commit. Basic skeleton built, assets and project directory setup. Header/Navbar built.
 
 Commit #2 - Coded Hero Section - Html & CSS. Added proper Readme.md file.
+
+Commit #3 - Coded JS Parallax Effect and tied into heroSection bg.
--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
 // /* == Vanilla JavaScript == */
 // alert("hello");
 
-// /* == jQuery == */
-// $(document).ready(function(){
-//     alert("hello");
-// });
+/* == jQuery == */
+$(document).ready(function(){
+   // Cache the Window Object.
+   var $window = $(window);
+
+   // Parallax BG Effect
+    $('section[data-type="background"]').each(function() {
+        var $bgobj = $(this); // assigning this object.
+        $(window).scroll(function() {
+            // scroll the bg at var speed
+            // the yPos is a negative value because we're scrolling it up.
+            var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+
+            // put together our final bg pos.
+            var coords = '50% ' + yPos + 'px';
+
+            // move the bg.
+            $bgobj.css({backgroundPosition: coords});
+        }); // end window scroll.
+    });
+});
